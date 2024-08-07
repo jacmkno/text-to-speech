2f764a07828f71749108351d793c1474944adba0
jacmkno <jose@activisual.net>
2024-08-07 07:18:27 +0000

    coqui_tts server demo

---
+++ coqui_tts/docker.sh
@@ -0,0 +1,4 @@
+# https://github.com/coqui-ai/TTS
+docker run --rm -it -p 5002:5002 --entrypoint /bin/bash ghcr.io/coqui-ai/tts-cpu
+# >> python3 TTS/server/server.py --list_models #To get the list of available models
+# >> python3 TTS/server/server.py --model_name tts_models/en/vctk/vits # To start a server
